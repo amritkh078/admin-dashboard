0b45cf946bd1f94944057735baa926992009c9be
amritkh078 <amritkhatiwada078@gmail.com>
2023-01-26 22:18:08 +0545

    routes created

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import clientRoute from './routes/client';
+import generalRoute from './routes/general';
+import managementRoute from './routes/management';
+import salesRoute from './routes/sales';
+
+// Configurations
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy:'cross-origin'}));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+//Routes
+app.use('/client', clientRoute);
+app.use('/general', generalRoute);
+app.use('/management', managementRoute);
+app.use('/sales', salesRoute);
